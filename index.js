4acf7841f7e626f0d61e136d05fe335835db9df8
Juan Pedraza <juan_pedraza@outlook.com>
2020-08-04 20:10:17 -0500

    Creating server

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const Hapi = require('@hapi/hapi');
+
+const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: 'localhost'
+})
+
+async function init (){
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (req,h) => {
+            return h.response('Hola mundo').code(200)
+        }
+    })
+    server.route({
+        method: 'GET',
+        path: '/redirect',
+        handler: (req,h) => {
+            return h.redirect('http://platzi.com')
+        }
+    })
+
+    try {
+        await server.start()
+    } catch (error){
+        console.error(error)
+        process.exit(1)
+    }
+    console.log(`Servidor lanzado en: ${server.info.uri} `);
+}
+
+init();
