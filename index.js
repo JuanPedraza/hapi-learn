e5f7cf3bb9822ab766deeec77628c8f78d63e147
Juan Pedraza <juan_pedraza@outlook.com>
2020-08-04 21:49:01 -0500

    Setup the project

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi');
+const handlerbars = require('handlebars')
 const inert = require('@hapi/inert');
 const path = require('path');
+const vision = require('@hapi/vision')
+
 
 const server = Hapi.server({
     port: process.env.PORT || 3000,
@@ -19,12 +22,44 @@ async function init (){
     try {
 
         await server.register(inert)
+        await server.register(vision)
+
+        server.views({
+            engines: {
+                hbs: handlerbars
+            },
+            relativeTo: __dirname, 
+            path: 'views',
+            layout: true,
+            layoutPath: 'views'
+        })
 
         server.route({
             method: 'GET',
-            path: '/home',
+            path: '/',
             handler: (req,h) => {
-                return h.file('index.html')
+                return h.view('index', {
+                    title: 'home'
+                })
+            }
+        })
+
+        server.route({
+            method: 'GET',
+            path: '/register',
+            handler: (req,h) => {
+                return h.view('register', {
+                    title: 'Registro'
+                })
+            }
+        })
+
+        server.route({
+            method: 'POST',
+            path: '/create-user',
+            handler: (req,h) => {
+                console.log(req.payload)
+                return 'usuario creado'
             }
         })
 
